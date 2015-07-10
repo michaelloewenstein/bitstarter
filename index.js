b09b38d72ed7df44b88a37eedb5be2fd7b9a1ce7
michaelloewenstein <ubuntu@ip-172-31-7-114.us-west-2.compute.internal>
2015-07-10 14:04:32 +0000

    using buffer

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World22!')
+  var fs = require("fs");
+  var filename = "index.html";
+  var buf = fs.readFileSync(filename, "utf8");
+  response.send(buf.toString('ascii'));
 })
 
 app.listen(app.get('port'), function() {
